c96e6ddcee4b5317c83599d1a83e7a492d56b61b
Thomas Lujan <tlujan94@rams.colostate.edu>
2021-05-20 22:11:11 -0600

    Modify sidebar to be collapsable.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Explorer } from './components/explorer';
-import { Toolbar } from './components/toolbar';
+import { Sidebar } from './components/sidebar';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
     constructor(props) {
@@ -15,7 +15,7 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Toolbar addCircle={this.addCircle.bind(this)}/>
+                <Sidebar/>
                 <Explorer ref={this.explorer}/>
             </div>
         );
