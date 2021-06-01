618b1965882e18a56e60029efc9c8d14ff0660fc
Thomas Lujan <tlujan94@rams.colostate.edu>
2021-06-01 13:17:36 -0600

    Add max/min date selection constraints.

--- src/components/sidebar_utils/dateSelector.js
+++ src/components/sidebar_utils/dateSelector.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import DatePicker from "react-datepicker";
 
+const MAX_DATE = new Date('01/01/2020');
+const MIN_DATE = new Date('01/01/2020');
+
 export class DateSelector extends React.Component {
     constructor(props) {
         super(props);
@@ -15,7 +18,10 @@ export class DateSelector extends React.Component {
                     <div style={{display: 'table', width: '100%', borderBottom: '1px solid white'}}>
                         <h4 style={{display: 'table-cell'}}>Date:</h4>
                         <div style={{display: 'table-cell', textAlign: 'right'}}>
-                            <DatePicker selected={this.state.date} popperPlacement='bottom-end'/>
+                            <DatePicker selected={this.state.date}
+                                        maxDate={MAX_DATE}
+                                        minDate={MIN_DATE}
+                                        popperPlacement='bottom-end'/>
                         </div>
                     </div>
 
